a64fa00cbc102e4b38e1e50a1686b2bfebb85b8a
Srinath Avadhanula <savadhan@savadhan-deb4-64.dhcp.mathworks.com>
2008-11-18 13:16:26 -0500

    - moved to a more robust send scheme. Repeatedly send data till there is no more. Sometimes the data which you send() might not make it immediately all across...

---
+++ vimfiles/autoload/gdb/sockutils.py
@@ -0,0 +1,13 @@
+def sendData(conn, data):
+    # print 'seinding [%s]' % data
+    data_len = len(data)
+    total_sent = 0
+    while total_sent < data_len:
+        sent = conn.send(data[total_sent:])
+        if sent == 0:
+            raise RuntimeError, "Socket connection broken by client!"
+        total_sent += sent
+
+def recvData(conn, data):
+
+
